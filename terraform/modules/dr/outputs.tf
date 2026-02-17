27bebf96a2458aa29a044981336f807a41ff7179
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2026-02-17 08:27:08 +0530

    Delete terraform directory

--- terraform/modules/dr/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "failover_fqdn" {
-  value = local.enable_dns ? var.domain_name : null
-}
-
-output "primary_health_check_id" {
-  value = try(aws_route53_health_check.primary[0].id, null)
-}
